c6d0ce025b6fd18c9bf6641f7583a226f0801553
Justin Langhorst <justin@healthblocks.com>
2011-02-16 16:34:21 -0600

    add default beanstalkd address and Beanstalk::NotConnected error handling

--- lib/beantool.rb
+++ lib/beantool.rb
@@ -6,7 +6,7 @@ require 'beantool/monitoring'
 
 class Beantool
   NAME = 'beantool'
-  VERSION = '0.2.0'
+  VERSION = '0.2.1'
 
   include Beantool::Administering
   include Beantool::Monitoring
@@ -16,8 +16,12 @@ class Beantool
   end
 
   def initialize(addrs)
+    addrs << "0.0.0.0:11300" if addrs.empty?
     @pool = Beanstalk::Pool.new(addrs) 
     #@pool.ignore('default')
+  rescue Beanstalk::NotConnected
+    puts "FATAL: Unable to connect to beanstalkd"
+    exit
   end
 end
 
